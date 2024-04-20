707692d370a7c9302d8064c76d86250164e584db
senpaiblue <singhsakcham29@email.com>
2024-04-20 20:02:47 +0530

    background-setup

--- background.js
+++ background.js
@@ -1,12 +1,10 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if (tab.url && tab.url.includes("youtube.com/watch")) {
-      const queryParameters = tab.url.split("?")[1];
-      const urlParameters = new URLSearchParams(queryParameters);
-  
-      chrome.tabs.sendMessage(tabId, {
-        type: "NEW",
-        videoId: urlParameters.get("v"),
-      });
-    }
-  });
-  
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.url && changeInfo.url.includes("https://github.com/") && changeInfo.url.includes("/pull/")) {
+    const prNumber = parseInt(changeInfo.url.split("/pull/")[1], 10);
+
+    chrome.tabs.sendMessage(tabId, {
+      type: "NEW",
+      NumberOfPr: prNumber,
+    });
+  }
+});
