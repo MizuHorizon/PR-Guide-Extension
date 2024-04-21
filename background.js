2fa4d94d131b797bc1adbae85c8577a013ebada0
Anshuman Sharma <anshuman9998@gmail.com>
2024-04-21 12:38:04 +0530

    updated backgrounf.js

--- background.js
+++ background.js
@@ -1,7 +1,9 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.url && changeInfo.url.includes("/pull/") && changeInfo.url.includes("/files")) {
-    chrome.action.enable(tabId);
-  } else {
-    chrome.action.disable(tabId);
+  if (changeInfo.status === "complete" && tab.url) {
+    if (tab.url.includes("/pull/") && tab.url.includes("/files")) {
+      chrome.action.setIcon({ tabId: tabId, path: "assets/bookmark.png" });
+    } else {
+      chrome.action.setIcon({ tabId: tabId, path: "assets/delete.png" });
+    }
   }
 });
